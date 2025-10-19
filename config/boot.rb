0986d6f553b175e05c7d074be2fc53d7b0863891
ErikRKT <erikfontinele@aluno.uespi.br>
2025-10-19 14:51:00 -0300

    Fix: Adiciona cache do bootsnap a todos os jobs e melhora o rescue

--- config/boot.rb
+++ config/boot.rb
@@ -2,7 +2,7 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 begin
-  require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
-rescue LoadError => e
+  require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+rescue StandardError => e
   warn "bootsnap not loaded: #{e.message}" # allow boot to proceed in CI
 end
